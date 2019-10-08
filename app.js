793396e009449c7f32790c8f18fa53dd0c5d0a98
ithaqua974 <tristan.hoarau@gmail.com>
2019-10-08 10:35:29 +0400

    app.js

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ let path = require('path');
 let ejs = require('ejs');
 
 let app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// Setup serveur local
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server started on port http://localhost:' + port + '/');
+});
